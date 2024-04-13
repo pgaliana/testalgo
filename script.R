f9342b9a3c6af3d6a934ca491b9a9aee42161fcf
Pablo Galiana <pgaliana@gmail.com>
2024-04-13 10:06:58 -0300

    Agregar script.R
    - se agrega script R
    - se agrega archivo de texto

---
+++ script.R
@@ -0,0 +1,10 @@
+# script.R
+
+
+# Determinar el directorio del script actual
+# y cambiar el working directory al directorio del script actual
+script_path <- file.path(dirname(rstudioapi::getSourceEditorContext()$path), "")
+setwd(script_path)
+
+options(scipen=999)
+rm(list = ls())
